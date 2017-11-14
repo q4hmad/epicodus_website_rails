4f80b35fc8e73b9e89f405e1b80fcb0726d4f5a3
Qudsia Ahmad and Rebecca McIntosh <qudsia@>
2017-11-14 13:49:45 -0800

    added edit and delete funcitons

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -20,6 +20,27 @@ class CoursesController < ApplicationController
     end
   end
 
+  def edit
+    @course = Course.find(params[:id])
+  end
+
+  def update
+    @course= Course.find(params[:id])
+    if @course.update(course_params)
+      redirect_to courses_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @course = Course.find(params[:id])
+    @course.destroy
+    redirect_to courses_path
+  end
+
+
+
 private
   def course_params
     params.require(:course).permit(:name)
